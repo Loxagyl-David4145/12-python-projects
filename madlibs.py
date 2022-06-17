5d67958d1b24ccd67b8c193d449918d642f727d6
Shedrack David <loxagyldavid1245@gmail.com>
2022-06-17 06:46:22 +0100

    first commit

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+adj = input("Enter an adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person =input("Famous person: ")
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. stay hydrated and {verb2} like I am {famous_person}!"
+
+print(madlib)
---
+++ zombie.py
@@ -0,0 +1,1 @@
+# Zomibies.py
