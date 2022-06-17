c219b98240f31e1b12403b61b029c7c516a5ccae
Shedrack David <loxagyldavid1245@gmail.com>
2022-06-17 07:29:46 +0100

    latest upload madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -5,4 +5,5 @@ famous_person =input("Famous person: ")
 madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
 I love to {verb1}. stay hydrated and {verb2} like I am {famous_person}!"
 
+
 print(madlib)
