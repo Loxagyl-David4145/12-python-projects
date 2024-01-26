e3843a36053db78e7585a82eb29763241953d93e
Shedrack David <loxagyldavid1245@gmail.com>
2024-01-26 13:34:27 +0100

    Updating GUI
    Adding GUI using wxpython widgets

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
+import wolframalpha
+import wikipedia as wk
+import wikipediaapi as kw
+import wx
+class MyFrame(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, None,
+            pos=wx.DefaultPosition, size=wx.Size(550,100),
+            style=wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION |
+            wx.CLOSE_BOX | wx.CLIP_CHILDREN,
+            title="PyDa")
+        panel = wx.Panel(self)
+        my_sizer = wx.BoxSizer(wx.VERTICAL)
+        lbl = wx.StaticText(panel,
+        label="Hello I'm PyDa, Your Basic Virtual P.A, How can I help you?")
+        my_sizer.Add(lbl, 0, wx.ALL, 5)
+        self.txt = wx.TextCtrl(panel, style=wx.TE_PROCESS_ENTER, size=(400,30))
+        self.txt.SetFocus()
+        self.txt.Bind(wx.EVT_TEXT_ENTER, self.OnEnter)
+        my_sizer.Add(self.txt, 0, wx.ALL, 5)
+        panel.SetSizer(my_sizer)
+        self.Show()
 
-import random
+    def OnEnter(self, event):
+        brand = self.txt.GetValue()
+        user_input = brand.lower()
+        try:
+        #wolframalpha
+            app_id = "HWWEQW-2J96G39LYU"
+            client = wolframalpha.Client(app_id)
+            result = client.query(user_input)
+            answer = next(result.results).text
+            print(answer)
+        except:
+            #wikipedia
+            print(wk.summary(user_input))
+            wiki_wiki = kw.Wikipedia("Thomas Edison", 'en')
+            print(wiki_wiki)
 
-def play():
-    user = input("'R' for rock, 'P' for paper, 'S' for scissors\n")
-    computer = random.choice(['r', 'p', 's'])
-    if user == computer:
-        return 'It\'s a tie'
 
-    if is_win(user, computer):
-        return 'You won!'
-
-    return 'You lost!'
-
-
-def is_win(player, opponent):
-        if (player == 'R' and opponent == 'S') or (player == 'S' and opponent == 'P') or (player == 'P' and opponent == 'R'):
-            return True
-
-print(play())
+if __name__ == "__main__":
+    app = wx.App(True)
+    frame = MyFrame()
+    app.MainLoop()
--- pyda.py
+++ pyda.py
@@ -1,17 +1,44 @@
 import wolframalpha
 import wikipedia as wk
 import wikipediaapi as kw
+import wx
+class MyFrame(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, None,
+            pos=wx.DefaultPosition, size=wx.Size(550,100),
+            style=wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION |
+            wx.CLOSE_BOX | wx.CLIP_CHILDREN,
+            title="PyDa")
+        panel = wx.Panel(self)
+        my_sizer = wx.BoxSizer(wx.VERTICAL)
+        lbl = wx.StaticText(panel,
+        label="Hello I'm PyDa, Your Basic Virtual P.A, How can I help you?")
+        my_sizer.Add(lbl, 0, wx.ALL, 5)
+        self.txt = wx.TextCtrl(panel, style=wx.TE_PROCESS_ENTER, size=(400,30))
+        self.txt.SetFocus()
+        self.txt.Bind(wx.EVT_TEXT_ENTER, self.OnEnter)
+        my_sizer.Add(self.txt, 0, wx.ALL, 5)
+        panel.SetSizer(my_sizer)
+        self.Show()
 
-while True:
-    user_input = input("Question: ")
-    try:
-        #wolframalpha
-        app_id = "HWWEQW-2J96G39LYU"
-        client = wolframalpha.Client(app_id)
-        result = client.query(user_input)
-        answer = next(result.results).text
-        print(answer)
-    except:
-        #wikipedia
-        print(wk.summary(user_input))
-        wiki_wiki = kw.Wikipedia("Thomas Edison", 'en')
+    def OnEnter(self, event):
+        brand = self.txt.GetValue()
+        user_input = brand.lower()
+        try:
+            #wolframalpha
+            app_id = "HWWEQW-2J96G39LYU"
+            client = wolframalpha.Client(app_id)
+            result = client.query(user_input)
+            answer = next(result.results).text
+            print(answer)
+        except:
+            #wikipedia
+            print(wk.summary(user_input))
+            wiki_wiki = kw.Wikipedia("Thomas Edison", 'en')
+            print(wiki_wiki)
+
+
+if __name__ == "__main__":
+    app = wx.App(True)
+    frame = MyFrame()
+    app.MainLoop()
